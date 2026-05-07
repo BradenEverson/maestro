4f1da22133bbca8e96a83a3c77de2e286b509bec
BradenEverson <bradeneverson@gmail.com>
2026-05-07 10:25:13 -0500

    Bring in a lil sample midi

--- midi/src/main.zig
+++ midi/src/main.zig
@@ -2,5 +2,27 @@ const std = @import("std");
 const midi = @import("midi");
 
 pub fn main(init: std.process.Init) !void {
-    _ = init;
+    const io = init.io;
+    const alloc = init.gpa;
+
+    var args = init.minimal.args.iterate();
+    _ = args.next();
+
+    var source: []u8 = undefined;
+
+    if (args.next()) |file_path| {
+        source = try std.Io.Dir.cwd().readFileAlloc(
+            io,
+            file_path,
+            alloc,
+            .unlimited,
+        );
+    } else {
+        std.debug.print("Missing MIDI file!!!\n", .{});
+        std.process.exit(1);
+    }
+
+    errdefer alloc.free(source);
+
+    std.debug.print("{X}\n", .{source});
 }
