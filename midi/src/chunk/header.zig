ea82d11ef0a36e43831b244d6e8a5ce4910692a8
BradenEverson <bradeneverson@gmail.com>
2026-05-07 10:44:15 -0500

    Begin header

--- midi/src/chunk/header.zig
+++ midi/src/chunk/header.zig
@@ -1,22 +1,27 @@
 //! Header Chunk Definition
 
-/// The overall organization of the MIDI file. Only three values are valid, making most of the 16
+/// The overall organization of the MIDI file.
+/// Only three values are valid, making most of the 16
 /// bits irrelevant
-pub const Format = enum {
+pub const Format = enum(u16) {
     /// The file contains a single multi-channel track
-    Zero,
-    /// The file contains one or more simultaneous tracks (or MIDI outputs) of a sequence
-    One,
-    /// The file contains one or more sequentially independent single-track patterns
-    Two,
+    Zero = 0,
+    /// The file contains one or more simultaneous
+    /// tracks (or MIDI outputs) of a sequence
+    One = 1,
+    /// The file contains one or more sequentially
+    /// independent single-track patterns
+    Two = 2,
 };
 
 /// The meaning of the delta-times in the MIDI sequence,
 pub const Division = union(enum) {
-    /// When bit 15 is a 0, bits 14-0 represent ticks per quarter note
+    /// When bit 15 is a 0, bits 14-0 represent
+    /// ticks per quarter note
     metrical: u16,
-    /// When bit 15 is 1, bits 14-8 represent the negative SMPTE format,
-    /// and bits 7-0 represent ticks per frame
+    /// When bit 15 is 1, bits 14-8 represent the
+    /// negative SMPTE format, and bits 7-0 represent
+    /// ticks per frame
     time_code: SmpteTicks,
 };
 
@@ -37,7 +42,9 @@ division: Division,
 
 const Header = @This();
 
-pub fn fromBytes(bytes: []const u8) !struct { Header, []const u8 } {
+pub fn fromBytes(
+    bytes: []const u8,
+) !struct { Header, []const u8 } {
     _ = bytes;
     return error.TODO;
 }
