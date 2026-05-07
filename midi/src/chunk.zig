189307e70bd7e597966b4f33a3f67d5e65d9a50d
BradenEverson <bradeneverson@gmail.com>
2026-05-07 10:05:58 -0500

    Basic structure of the midi parser

---
+++ midi/src/chunk.zig
@@ -0,0 +1,1 @@
+//! A MIDI chunk :D
--- midi/src/midi.zig
+++ midi/src/midi.zig
@@ -1,1 +1,23 @@
 //! MIDI File Parsing
+
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+
+pub const Chunk = @import("chunk.zig");
+
+chunks: []const Chunk,
+
+const MIDI = @This();
+
+pub fn deinit(midi: *MIDI, alloc: Allocator) void {
+    alloc.free(midi.chunks);
+}
+
+pub fn tryFromBytes(bytes: []const u8) !MIDI {
+    _ = bytes;
+
+    // TODO
+    return .{
+        .chunks = &[0]Chunk{},
+    };
+}
