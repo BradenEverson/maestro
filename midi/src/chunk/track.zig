6ba70e5fb7dbf7399185da2446b30904887f380a
BradenEverson <bradeneverson@gmail.com>
2026-05-15 16:33:44 -0500

    Header's a bit weird...

--- midi/src/chunk/track.zig
+++ midi/src/chunk/track.zig
@@ -1,10 +1,20 @@
 //! Track Chunk Definition
 
 const std = @import("std");
+const chunk = @import("../chunk.zig");
 
 const Track = @This();
 
+mtrk_events: std.ArrayList(MTrkEvent),
+
 pub fn fromBytes(bytes: []const u8) !struct { Track, []const u8 } {
-    _ = bytes;
+    const chunk_header = bytes[0..4];
+    std.debug.print("{s}\n", .{chunk_header});
+
+    if (!std.mem.eql(u8, chunk_header, chunk.TRACK_TAG))
+        return error.WrongTag;
+
     return error.TODO;
 }
+
+pub const MTrkEvent = struct {};
