f56566293292655f9a0712503b8169ec2dcfec52
BradenEverson <bradeneverson@gmail.com>
2026-05-18 11:45:50 -0500

    Midi Events

--- midi/src/chunk/track.zig
+++ midi/src/chunk/track.zig
@@ -7,18 +7,48 @@ const Track = @This();
 
 mtrk_events: std.ArrayList(MTrkEvent),
 
-pub fn fromBytes(bytes: []const u8) !struct { Track, []const u8 } {
+pub fn fromBytes(
+    bytes: []const u8,
+) !struct {
+    Track,
+    []const u8,
+} {
     const chunk_header = bytes[0..4];
 
-    if (!std.mem.eql(u8, chunk_header, chunk.TRACK_TAG))
+    if (!std.mem.eql(
+        u8,
+        chunk_header,
+        chunk.TRACK_TAG,
+    ))
         return error.WrongTag;
 
     var rest = bytes[4..];
-    const len, rest = chunk.bufferedIntRead(u32, rest);
+    const len, rest =
+        chunk.bufferedIntRead(u32, rest);
 
     std.debug.print("{} bytes\n", .{len});
 
     return error.TODO;
 }
 
-pub const MTrkEvent = struct {};
+pub const MTrkEvent = struct {
+    delta_time: u32,
+    event: Event,
+};
+
+pub const Event = union(enum) {
+    midi: MidiEvent,
+    // We do not care about the other variants for now :(
+    // sysex and meta events :(((
+};
+
+pub const MidiEvent = union(enum) {
+    note_off: struct { u8, NoteMeta },
+    note_on: struct { u8, NoteMeta },
+    // We do not care about the other variants for now :(
+};
+
+pub const NoteMeta = struct {
+    key: u8,
+    velocity: u8,
+};
