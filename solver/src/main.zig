7e3e1f333456298ca918f5121bf3090bdc2fd943
BradenEverson <bradeneverson@gmail.com>
2026-05-27 09:45:52 -0500

    Define structure of the solver, piano, and a little formula for identifying a black key (which requires octave alignment)

--- solver/src/main.zig
+++ solver/src/main.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const Solver = @import("solver");
+const Solver = @import("solver").Solver;
 
 pub fn main(init: std.process.Init) !void {
     const io = init.io;
--- solver/src/root.zig
+++ solver/src/root.zig
@@ -1,20 +1,52 @@
-//! Solver responsible for translating sequence of MIDI events into maestro events that can include moving the servos
+//! Solver responsible for translating sequence of MIDI events
+//! into maestro events that can include moving the servos
 
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 
 const Midi = @import("midi");
 
-stream: Midi,
+/// Black keys require that a hand be octave
+/// aligned, if we need to reach one we must
+/// ensure this alignment before knowing we
+/// can actuate
+fn isBlackKey(key: usize) bool {
+    const black_keys_octave =
+        &[_]usize{ 1, 3, 6, 8, 10 };
 
-const Solver = @This();
+    const octave_idx = key % 12;
 
-pub fn init(alloc: Allocator, bytes: []const u8) !Solver {
-    return .{
-        .stream = try Midi.fromBytes(alloc, bytes),
-    };
+    return std.mem.find(usize, black_keys_octave, &[_]usize{octave_idx}) != null;
 }
 
-pub fn deinit(solver: *Solver, alloc: Allocator) void {
-    solver.stream.deinit(alloc);
+pub const Solver = struct {
+    stream: Midi,
+    piano_len: usize = 61,
+
+    // First hand homes on the leftmost spot
+    hand1: usize = 0,
+    // Second hand homes on the rightmost spot
+    hand2: usize = 60 - HAND_SIZE,
+
+    const HAND_SIZE: usize = 12;
+
+    pub fn init(alloc: Allocator, bytes: []const u8) !Solver {
+        return .{
+            .stream = try Midi.fromBytes(alloc, bytes),
+        };
+    }
+
+    pub fn deinit(solver: *Solver, alloc: Allocator) void {
+        solver.stream.deinit(alloc);
+    }
+};
+
+test "black key identification" {
+    // Selecting the highest octave
+    try std.testing.expect(isBlackKey(58));
+    try std.testing.expect(isBlackKey(56));
+    try std.testing.expect(isBlackKey(54));
+
+    try std.testing.expect(isBlackKey(51));
+    try std.testing.expect(isBlackKey(49));
 }
