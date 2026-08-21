930c2dd49edccf12eab17eaf6fda73a480169700
BradenEverson <bradeneverson@gmail.com>
2026-08-21 14:27:47 -0500

    I think UART works?

--- righthand_esp32/main/app.zig
+++ righthand_esp32/main/app.zig
@@ -1,8 +1,32 @@
 const std = @import("std");
 const builtin = @import("builtin");
 const idf = @import("esp_idf");
+const sys = idf.sys;
 
 const log = std.log.scoped(.right_hand_man);
+
+const UART_PORT: c_uint = 1; // UART1
+const BAUD_RATE = 115200;
+const BUF_SIZE = 256;
+
+const TX_PIN: c_int = 43;
+const RX_PIN: c_int = 44;
+
+pub fn setPin(port: c_uint, pins: struct {
+    tx: c_int = sys.UART_PIN_NO_CHANGE,
+    rx: c_int = sys.UART_PIN_NO_CHANGE,
+    rts: c_int = sys.UART_PIN_NO_CHANGE,
+    cts: c_int = sys.UART_PIN_NO_CHANGE,
+}) !void {
+    const ret = sys._uart_set_pin4(
+        port,
+        pins.tx,
+        pins.rx,
+        pins.rts,
+        pins.cts,
+    );
+    if (ret != sys.ESP_OK) return error.SetPinFailed;
+}
 
 export fn app_main() callconv(.c) void {
     var heap: idf.heap.VPortAllocator = .init();
@@ -10,10 +34,22 @@ export fn app_main() callconv(.c) void {
 
     _ = alloc;
 
-    log.info("I'm the right hand!!!", .{});
+    setPin(UART_PORT, .{
+        .tx = TX_PIN,
+        .rx = RX_PIN,
+    }) catch unreachable;
 
+    idf.uart.driverInstall(UART_PORT, .{
+        .rx_buffer_size = BUF_SIZE * 2,
+        .tx_buffer_size = 0,
+    }) catch unreachable;
+
+    var buf: [BUF_SIZE]u8 = undefined;
     while (true) {
-        idf.rtos.Task.delayMs(100);
+        const n = idf.uart.readBytes(UART_PORT, &buf, sys.portMAX_DELAY) catch unreachable;
+        if (n > 0) {
+            _ = idf.uart.writeBytes(UART_PORT, buf[0..n]) catch unreachable;
+        }
     }
 }
 
